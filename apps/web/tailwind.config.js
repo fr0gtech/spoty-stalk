cc6d3e6f5da6f02a278ac627451341b5426c0c1d
frogtech0 <techfrog0@gmail.com>
2022-12-19 18:02:18 +0100

    fix(css): animation lagged out on lower spec pc

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,54 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-        'animation': {
-            'gradient-x':'gradient-x 5s ease infinite',
-            'gradient-y':'gradient-y 5s ease infinite',
-            'gradient-xy':'gradient-xy 5s ease infinite',
-            border: 'border 4s ease infinite',
-        },
-        'keyframes': {
-            border: {
-                '0%, 100%': { backgroundPosition: '0% 50%' },
-                '50%': { backgroundPosition: '100% 50%' },
-              },
-            'gradient-y': {
-                '0%, 100%': {
-                    'background-size':'400% 400%',
-                    'background-position': 'center top'
-                },
-                '50%': {
-                    'background-size':'200% 200%',
-                    'background-position': 'center center'
-                }
-            },
-            'gradient-x': {
-                '0%, 100%': {
-                    'background-size':'200% 200%',
-                    'background-position': 'left center'
-                },
-                '50%': {
-                    'background-size':'200% 200%',
-                    'background-position': 'right center'
-                }
-            },
-            'gradient-xy': {
-                '0%, 100%': {
-                    'background-size':'400% 400%',
-                    'background-position': 'left center'
-                },
-                '50%': {
-                    'background-size':'200% 200%',
-                    'background-position': 'right center'
-                }
-            }
-        }
-    }
-},
-  plugins: [],
-}
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
