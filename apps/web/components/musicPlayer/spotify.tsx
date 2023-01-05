d698f38ae76985ceb3acd2b7349a47aaccf54de2
lucavin <lucavin@gmail.com>
2023-01-05 21:01:05 +1100

    Changed player name that appears on Spotify interface.

--- apps/web/components/musicPlayer/spotify.tsx
+++ apps/web/components/musicPlayer/spotify.tsx
@@ -139,7 +139,7 @@ function SpotifyPlayer(props: { token: string }) {
 
     window.onSpotifyWebPlaybackSDKReady = () => {
       const spotifyPlayer = new window.Spotify.Player({
-        name: "Web Playback SDK!!!",
+        name: "pokeING",
         getOAuthToken: (cb) => {
           cb(props.token);
         },
